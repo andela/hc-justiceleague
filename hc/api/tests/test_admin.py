63c92fc8398c6556e2e83ac4a95f88f39930152d
Salym Senyonga <salymsash@gmail.com>
2017-11-30 08:15:20 +0300

    [ch #153112486] Make test alice staff and superuser
    The API tests contain hints on the steps to be taken as on expands and
    improves on the tests. One of the hints is to make the test user Alice
    in `hc/api/tests/test_admin.py` a staff and super user.
    
    This commit adds the required update.

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -1,6 +1,8 @@
 from hc.api.models import Channel, Check
 from hc.test import BaseTestCase
 
+from django.contrib.auth.models import User
+
 
 class ApiAdminTestCase(BaseTestCase):
 
@@ -9,6 +11,16 @@ class ApiAdminTestCase(BaseTestCase):
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
 
         ### Set Alice to be staff and superuser and save her :)
+        self.assertFalse(self.alice.is_superuser)
+        self.assertFalse(self.alice.is_staff)
+
+        self.alice.is_staff     = True
+        self.alice.is_superuser = True
+        self.alice.save()
+        retrieved_alice         = User.objects.get(username='alice')
+
+        self.assertTrue(retrieved_alice.is_superuser)
+        self.assertTrue(retrieved_alice.is_staff)
 
     def test_it_shows_channel_list_with_pushbullet(self):
         self.client.login(username="alice@example.org", password="password")
