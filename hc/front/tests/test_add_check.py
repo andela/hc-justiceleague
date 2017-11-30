83bb8aae74a8318bdbb96c99354ce066cd6c149f
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2017-11-30 15:13:36 +0300

    [Chore #153112485]Adding comments to test_add_check.py

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -13,7 +13,7 @@ class AddCheckTestCase(BaseTestCase):
 
     ### Test that team access works
     def test_team_access(self):
-        # Create a check
+        # Create a check with Alice 
         url = "/checks/add/"
         self.client.login(username="alice@example.org", password="password")
         r = self.client.post(url)
@@ -21,10 +21,13 @@ class AddCheckTestCase(BaseTestCase):
         check = Check.objects.filter(user=self.alice).first().code
         assert Check.objects.count() == 1
         self.client.logout()
+        """login Bob who has access to  Alice`s stuff there fore the 
+        check created by alice should be viewed by Bob """
         self.client.login(username="bob@example.org", password="password")
         response = self.client.get('/checks/')
         self.assertIn(str(check), str(response.content))
         self.client.logout()
+        #login with Charlie who has no access to Alice`s stuff
         self.client.login(username="charlie@example.org", password="password")
         charlie_s_checks = self.client.get('/checks/')
         self.assertContains(charlie_s_checks, "You don\'t have any checks yet.")
