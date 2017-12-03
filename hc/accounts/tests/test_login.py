a9c8f5777d702e3480b42d777c14eb02d276a685
Betty-Kebenei <kebeneibetty13@gmail.com>
2017-12-03 18:50:00 +0300

    [Chore #153112484] Assert that a user was created, Assert contents of the email body and Assert that check is associated with the new user in test_login file

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.core import mail
 from django.test import TestCase
 from hc.api.models import Check
+from django.utils.html import escape
 
 
 class LoginTestCase(TestCase):
@@ -17,16 +18,22 @@ class LoginTestCase(TestCase):
         form = {"email": "alice@example.org"}
 
         r = self.client.post("/accounts/login/", form)
-        assert r.status_code == 301
+        assert r.status_code == 302
 
         ### Assert that a user was created
+        self.assertEqual(User.objects.count(), 1)
 
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Log in to healthchecks.io')
+
         ### Assert contents of the email body
+        self.assertTrue("alice@example.org" in mail.outbox[0].to)
+        self.assertIn('To log into healthchecks.io', mail.outbox[0].body)
 
         ### Assert that check is associated with the new user
+        self.assertEqual(Check.objects.count(), 1)
+        self.assertTrue(check.code)
 
     def test_it_pops_bad_link_from_session(self):
         self.client.session["bad_link"] = True
@@ -34,4 +41,7 @@ class LoginTestCase(TestCase):
         assert "bad_link" not in self.client.session
 
         ### Any other tests?
+
+
+    
 
