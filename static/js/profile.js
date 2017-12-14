970ea42e665f45d4ff4373b8d00f0a38bb64cf58
Betty-Kebenei <kebeneibetty13@gmail.com>
2017-12-14 12:21:32 +0300

    [Feature #153112488] Add jquery to either show or hide radio buttons for choosing whether to receive the reports daily, weekly or monthly

--- static/js/profile.js
+++ static/js/profile.js
@@ -10,4 +10,20 @@ $(function() {
         return false;
     });
 
+    $("#showMyLabels").click(function(event) {
+        var button = $("#showradiobuttons");
+       if(event.target.checked){
+            button.show()
+            //avoid executing the hiding line below
+            return;
+       }
+
+       //uncheck any checked radio buttons
+        $(".report_buttons").each(function(index, element){
+            element.checked = false;
+        });
+        //hide the radio buttons
+        button.hide()
+    });
+
 });
